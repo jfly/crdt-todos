3e703ba487a20df7c4f2d710790fff503562685b
Scott Erickson <sderickson@gmail.com>
2025-10-03 14:04:33 -0700

    get things basically working

--- services/notes/notes-sdk/pages/note-list/NoteList.vue
+++ services/notes/notes-sdk/pages/note-list/NoteList.vue
@@ -8,8 +8,27 @@
 <script setup lang="ts">
 import { note_list_page as strings } from "./NoteList.strings";
 import { useReverseT } from "../../i18n.ts";
+import type { AnyDocumentId, DocHandle } from "@automerge/automerge-repo";
 
+const load = async () => {
+  let handle: DocHandle<any>;
+  const firstNote = localStorage.getItem("first-note");
+  if (firstNote) {
+    console.log("loading note", firstNote)
+    handle = await window.repo.find(firstNote as unknown as AnyDocumentId);
+  }
+  else {
+    console.log("creating note", "first-note")
+    handle = window.repo.create({
+      id: "first-note",
+      contents: "Hello, world!",
+    });
+    localStorage.setItem("first-note", handle.url);
+  }
 
+  console.log("handle", handle.url)
+}
+load();
 
 const { t } = useReverseT();
 </script>
