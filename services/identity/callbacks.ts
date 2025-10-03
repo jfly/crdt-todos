8df1d8941d93b9d2710a6667c4d1bab247f5a1c5
Scott Erickson <sderickson@gmail.com>
2025-10-03 15:48:15 -0700

    fix types

--- services/identity/callbacks.ts
+++ services/identity/callbacks.ts
@@ -1,33 +1,39 @@
-import type { User } from "@saflib/identity";
 import { mockingOn } from "@saflib/email";
 import { getSafReporters } from "@saflib/node";
 import type { IdentityServiceCallbacks } from "@saflib/identity";
+import type {
+  PasswordResetPayload,
+  PasswordUpdatedPayload,
+  VerificationTokenCreatedPayload,
+} from "@saflib/identity-common";
 
 // TODO: Set up email sends for proper email verification. See @saflib/email for more details.
 
-async function onVerificationTokenCreated(user: User, verificationUrl: string) {
+async function onVerificationTokenCreated(
+  payload: VerificationTokenCreatedPayload,
+) {
   const { log } = getSafReporters();
   // TODO: add your own email with `npm exec saf-workflow kickoff add-email-template ./emails/verify-email.ts`
-  log.info(`Verification email successfully sent to ${user.id}`);
+  log.info(`Verification email successfully sent to ${payload.user.id}`);
   if (mockingOn) {
-    log.info(`Link: ${verificationUrl}`);
+    log.info(`Link: ${payload.verificationUrl}`);
   }
 }
 
-async function onPasswordReset(user: User, resetUrl: string) {
+async function onPasswordReset(payload: PasswordResetPayload) {
   const { log } = getSafReporters();
   // TODO: add your own email with `npm exec saf-workflow kickoff add-email-template ./emails/reset-password.ts`
-  log.info(`Password reset email successfully sent to ${user.id}`);
+  log.info(`Password reset email successfully sent to ${payload.user.id}`);
   if (mockingOn) {
-    log.info(`Link: ${resetUrl}`);
+    log.info(`Link: ${payload.resetUrl}`);
   }
 }
 
-async function onPasswordUpdated(user: User) {
+async function onPasswordUpdated(payload: PasswordUpdatedPayload) {
   const { log } = getSafReporters();
   // TODO: add your own email with `npm exec saf-workflow kickoff add-email-template ./emails/password-updated.ts`
   log.info(
-    `Password update confirmation email successfully sent to ${user.id}`,
+    `Password update confirmation email successfully sent to ${payload.user.id}`,
   );
 }
 
--- services/notes/notes-sdk/typed-fake.ts
+++ services/notes/notes-sdk/typed-fake.ts
@@ -1,14 +1,6 @@
 import { typedCreateHandler } from "@saflib/sdk/testing";
 import type { paths } from "notes-spec";
-import { 
-  getNotesFake, 
-  getNoteFake, 
-  createNoteFake, 
-  updateNoteFake, 
-  deleteNoteFake 
-} from "./requests/notes.fakes.ts";
 
-export const { createHandler: notesHandler } =
-  typedCreateHandler<paths>({
-    subdomain: "notes",
-  });
+export const { createHandler: notesHandler } = typedCreateHandler<paths>({
+  subdomain: "notes",
+});
