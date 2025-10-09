d616e79a1d0a501965e8bb4d3113ce5338812fe4
Scott Erickson <sderickson@gmail.com>
2025-10-09 13:42:42 -0700

    use broadcast channel for network

--- services/notes/notes-sdk/repo.ts
+++ services/notes/notes-sdk/repo.ts
@@ -1,8 +1,10 @@
+import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
 import { Repo } from "@automerge/automerge-repo";
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
 
 const repo = new Repo({
   storage: new IndexedDBStorageAdapter(),
+  network: [new BroadcastChannelNetworkAdapter()],
 });
 declare global {
   interface Window {
